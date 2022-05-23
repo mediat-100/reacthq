dfadfda6849a280bd2e18741ffc411eba7d82b97
mediat-100 <yusuftomiwa740@gmail.com>
2022-05-23 20:37:07 +0100

    initial commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,6 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // 1) Create a transporter
   const transporter = nodemailer.createTransport({
-    // service: 'Gmail',
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
